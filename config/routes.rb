1e5959e1293e0e47e3628d80f23ac82e6373d8e2
JokePenny <S-total-war@mail.ru>
2020-12-17 16:35:43 +0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'game' => 'game#index'
   get 'menu' => 'menu#index'
   post "game" => "menu#on_click_button_play"
+  post 'menu' => 'game#on_click_button_exit'
+  resources :game
 end
