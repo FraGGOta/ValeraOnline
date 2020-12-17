f30b584ea9fc3bdeb4247f05f16155f901dac926
SetLN10 <setln10@mail.ru>
2020-12-17 17:52:42 +0700

    Add registration

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,9 @@ class AccountsController < ApplicationController
 
   def create
     #render plain: params[:account].inspect
+    @account = Account.new(params.require(:form_account).permit(:login, :password))
+    @account.save
+    redirect_to @account
   end
 
   def on_click_button_submit
