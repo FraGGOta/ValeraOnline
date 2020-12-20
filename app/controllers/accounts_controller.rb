3aa6140a0fa5906f161309a476fcc58fceb6e35c
SetLN10 <setln10@mail.ru>
2020-12-20 14:40:13 +0700

    Add redirect after authorization

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,10 +4,14 @@ class AccountsController < ApplicationController
   end
 
   def create
-    #render plain: params[:account].inspect
+    # render plain: params[:form_account].inspect
     @account = Account.new(params.require(:form_account).permit(:login, :password))
     @account.save
-    redirect_to @account
+    puts
+    puts @account.id
+    puts
+    redirect_to "/menu"
+    # redirect_to @account
   end
 
   def on_click_button_submit
