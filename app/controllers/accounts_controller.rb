0a60c1c6827b6fb2b2025d21d9472ecc77713229
SetLN10 <setln10@mail.ru>
2020-12-20 18:34:48 +0700

    Add authorization system

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -4,18 +4,11 @@ class AccountsController < ApplicationController
   end
 
   def create
-    # render plain: params[:form_account].inspect
-    @account = Account.new(params.require(:form_account).permit(:login, :password))
+    @account = Account.new()
+    @account.login = params[:form_account][:nick]
+    @account.password = params[:form_account][:passwd]
     @account.save
-    puts
-    puts @account.id
     session[:user_id] = @account.id
-    puts
     redirect_to "/menu"
-    # redirect_to @account
-  end
-
-  def on_click_button_submit
-
   end
 end
