5da328d8b846f13a3c32cc523d0988d8a5091920
FraGGOta <ramus99.99@mail.ru>
2020-12-22 17:00:10 +0700

    add test

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it 'returns all users' do
+    # setup
+    #active_user = create(:user, active: true)
+    #non_active_user = create(:user, active: false)
+
+    # exercise
+    #result = User.active
+
+    # verify
+    #expect(result).to eq [active_user]
+
+    # teardown is handled for you by RSpec
+  end
 end
--- spec/requests/game_request_spec.rb
+++ spec/requests/game_request_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Games", type: :request do
+  context "GET /game unauthorized" do
+    it "the request is unauthorized" do
+      get '/game'
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
 
+  context "POST /go_job" do
+    it "the request is authorized" do
+      post '/go_job'
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
