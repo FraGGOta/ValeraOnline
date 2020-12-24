c5f056482e11e1a3891cdaac890029fb8eaece26
JokePenny <S-total-war@mail.ru>
2020-12-24 15:00:06 +0700

    fix struct tests

--- spec/controllers/menu_controller_spec.rb
+++ spec/controllers/menu_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe MenuController, type: :controller do
   describe ' POST /on_click_button_play when user no login' do
     it 'renders no content' do
       post 'on_click_button_play'
-      expect(response).to have_http_status(:no_content)
+      expect(response).to have_http_status(:found)
     end
   end
 
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,62 +2,64 @@ require 'rails_helper'
 require 'support/factory_bot'
 
 RSpec.describe Account, type: :model do
-  it 'returns info account' do
-    account = build(:account, login: 'Josh', password: 'asdasd')
-    expect(account.info_accunt).to eq 'Josh asdasd'
-  end
+  describe 'stats operation' do
+    it 'returns info account' do
+      account = build(:account, login: 'Josh', password: 'asdasd')
+      expect(account.info_accunt).to eq 'Josh asdasd'
+    end
 
-  it 'test set health' do
-    account = build(:account, health: 0)
-    account.health_set(10)
-    expect(account.health).to eq 10
-  end
+    it 'test set health' do
+      account = build(:account, health: 0)
+      account.health_set(10)
+      expect(account.health).to eq 10
+    end
 
-  it 'test set mana up zero' do
-    account = build(:account, mana: 0)
-    account.mana_set(10)
-    expect(account.mana).to eq 10
-  end
+    it 'test set mana up zero' do
+      account = build(:account, mana: 0)
+      account.mana_set(10)
+      expect(account.mana).to eq 10
+    end
 
-  it 'test set mana below zero' do
-    account = build(:account, mana: 0)
-    account.mana_set(-10)
-    expect(account.mana).to eq 0
-  end
+    it 'test set mana below zero' do
+      account = build(:account, mana: 0)
+      account.mana_set(-10)
+      expect(account.mana).to eq 0
+    end
 
-  it 'test set money up zero' do
-    account = build(:account, money: 0)
-    account.money_set(10)
-    expect(account.money).to eq 10
-  end
+    it 'test set money up zero' do
+      account = build(:account, money: 0)
+      account.money_set(10)
+      expect(account.money).to eq 10
+    end
 
-  it 'test set money below zero' do
-    account = build(:account, money: 0)
-    account.money_set(-10)
-    expect(account.money).to eq 0
-  end
+    it 'test set money below zero' do
+      account = build(:account, money: 0)
+      account.money_set(-10)
+      expect(account.money).to eq 0
+    end
 
-  it 'test set funny below zero' do
-    account = build(:account, funny: 0)
-    account.funny_set(-30)
-    expect(account.funny).to eq(-10)
-  end
+    it 'test set funny below zero' do
+      account = build(:account, funny: 0)
+      account.funny_set(-30)
+      expect(account.funny).to eq(-10)
+    end
 
-  it 'test set funny up zero' do
-    account = build(:account, funny: 0)
-    account.funny_set(30)
-    expect(account.funny).to eq 10
-  end
+    it 'test set funny up zero' do
+      account = build(:account, funny: 0)
+      account.funny_set(30)
+      expect(account.funny).to eq 10
+    end
 
-  it 'test set tired below zero' do
-    account = build(:account, tired: 0)
-    account.tired_set(-10)
-    expect(account.tired).to eq 0
-  end
+    it 'test set tired below zero' do
+      account = build(:account, tired: 0)
+      account.tired_set(-10)
+      expect(account.tired).to eq 0
+    end
 
-  it 'test set tired up 100' do
-    account = build(:account, tired: 0)
-    account.tired_set(1000)
-    expect(account.tired).to eq 100
+    it 'test set tired up 100' do
+      account = build(:account, tired: 0)
+      account.tired_set(1000)
+      expect(account.tired).to eq 100
+    end
   end
 end
